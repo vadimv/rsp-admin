95fc35ddce1ce87b17abcc82acd7c136e9c01c20
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-04-28 20:49:14 +0200

    Refactor

--- src/main/java/rsp/admin/crud/components/Admin.java
+++ src/main/java/rsp/admin/crud/components/Admin.java
@@ -83,7 +83,7 @@ public class Admin {
         return html(window().on("popstate",
                                 ctx ->
             ctx.eventObject().value("path").ifPresent(path -> dispatch(us.get().user, Path.of(path.toString()))
-                                                                         .thenAccept(us::accept)))
+                                                                         .thenAccept(us)))
         ,
                     head(title(title + us.get().currentResource.map(r -> ": " + r.title).orElse("")),
                          link(attr("rel", "stylesheet"), attr("href","/res/style.css"))),
